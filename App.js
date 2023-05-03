86d6a298bbc38a03e7876c4d229c3df05de466e5
Chris Harris <chopuk@gmail.com>
2023-05-03 19:46:03 +0100

    Initial

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import * as SplashScreen from 'expo-splash-screen'
+import HomeScreen from './screens/HomeScreen'
 
-export default function App() {
+// keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync()
+
+const App = () => {
+
+  // display the splash screen for 1 second...
+  setTimeout(async () => {
+    await SplashScreen.hideAsync()
+  }, 2000)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle = 'light-content' hidden ={false} backgroundColor = '#35a4e8' translucent ={true}/>
+      <HomeScreen/>
     </View>
-  );
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{
+    flex:1,
+    backgroundColor:'white'
+  }
+})
